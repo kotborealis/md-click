e1a1845abdee2f02f028d75bcbe4127455fdce0d
kotborealis <kotborealis@gmail.com>
2022-09-08 19:40:50 +0300

    Update main.py

--- md_click/main.py
+++ md_click/main.py
@@ -1,42 +1,98 @@
-import click
+#!/usr/bin/env python3
+
 import pathlib
 import importlib
+import itertools
+import os
 
-md_base_template = """
-# {command_name}
+import click
+
+# Edit this template to change markdown output
+md_base_template = """# {command_name}
 
 {description}
 
-## Usage
+**Usage:** `{usage}`
 
-```
-{usage}
-```
+**Options:**
 
-## Options
 {options}
-
-## CLI Help
-
-```
-{help}
-```
 
 """
 
 
+def trim_empty_lines(data: str):
+    """
+    Remove empty lines from start and end of text.
+    """
+    def empty(x):
+        return x == ""
+
+    lines = data.splitlines()
+    lines = list(itertools.dropwhile(empty, lines))
+    lines = reversed(lines)
+    lines = list(itertools.dropwhile(empty, lines))
+    lines = reversed(lines)
+    return '\n'.join(lines)
+
+
+def trim_docstring(data):
+    """
+    Remove common indentation from documentation string.
+    Example: "  Hello,\n   World!" -> "Hello,\n World!"
+             Lines have common indentation of 2 spaces,
+             which will be removed by this function.
+    """
+    lines = trim_empty_lines(data).splitlines()
+
+    print(lines)
+
+    common_indentation = min([
+        len(list(itertools.takewhile(
+            lambda x: x == ' ' or x == '\t',
+            line
+        ))) for line in lines if line
+    ])
+
+    return '\n'.join([line[common_indentation:] for line in lines])
+
+
 def recursive_help(cmd, parent=None):
+    """
+    Recursively get help options from command and it's children.
+    """
     ctx = click.core.Context(cmd, info_name=cmd.name, parent=parent)
 
-    yield {"command": cmd, "help": cmd.get_help(ctx), "parent": parent.info_name if parent else '',
-           "usage": cmd.get_usage(ctx),
-           "params": cmd.get_params(ctx),
-           "options": cmd.collect_usage_pieces(ctx)}
+    yield {
+        "command": cmd,
+        "help": cmd.get_help(ctx),
+        "parent": parent.info_name if parent else '',
+        "usage": cmd.get_usage(ctx),
+        "params": cmd.get_params(ctx),
+        "options": cmd.collect_usage_pieces(ctx)
+    }
 
     commands = getattr(cmd, 'commands', {})
     for sub in commands.values():
         for helpdct in recursive_help(sub, ctx):
             yield helpdct
+
+
+def format_option(opt):
+    usage = ', '.join(opt.get('usage').splitlines())
+    required = ' (REQUIRED)' if opt.get('required') else ''
+    default_value = opt.get('default', None)
+
+    # special case
+    if default_value == os.getcwd():
+        default_value = "<current directory>"
+
+    res = f"* `{usage}`{required}: {opt.get('help') or ''}\n"
+
+    if default_value is not None:
+        res += f"  * Default value: `{default_value}`\n"
+
+    return res
 
 
 def dump_helper(base_command, docs_dir):
@@ -46,33 +102,26 @@ def dump_helper(base_command, docs_dir):
         command = helpdct.get("command")
         helptxt = helpdct.get("help")
         usage = helpdct.get("usage")
-        parent = helpdct.get("parent", "") or ''
+
         options = {
             opt.name: {
                 "usage": '\n'.join(opt.opts),
-                "prompt": opt.prompt,
-                "required": opt.required,
-                "default": opt.default,
-                "help": opt.help,
-                "type": str(opt.type)
+                "prompt": getattr(opt, "prompt", None),
+                "required": getattr(opt, "required", None),
+                "default": getattr(opt, "default", None),
+                "help": getattr(opt, "help", None),
+                "type": str(getattr(opt, "type", None))
             }
             for opt in helpdct.get('params', [])
         }
-        full_command = f"{str(parent) + ' ' if parent else ''}{str(command.name)}"
 
         md_template = md_base_template.format(
-            command_name=full_command,
-            description=command.help,
+            command_name=command.name,
+            description=trim_docstring(command.help),
             usage=usage,
-            options="\n".join([
-                f"* `{opt_name}`{' (REQUIRED)' if opt.get('required') else ''}: \n"
-                f"  * Type: {opt.get('type')} \n"
-                f"  * Default: `{str(opt.get('default')).lower()}`\n"
-                f"  * Usage: `{opt.get('usage')}`\n"
-                "\n"
-                f"  {opt.get('help') or ''}\n"
-                f"\n"
-                for opt_name, opt in options.items()
+            options="".join([
+                format_option(opt)
+                for _, opt in options.items()
             ]),
             help=helptxt
         )
@@ -81,15 +130,17 @@ def dump_helper(base_command, docs_dir):
             # Create md file dir if needed
             docs_path.mkdir(parents=True, exist_ok=False)
 
-        md_file_path = docs_path.joinpath(full_command.replace(' ', '-').lower() + '.md').absolute()
+        md_file_path = docs_path.joinpath(command.name.replace(' ', '-').lower() + '.md').absolute()
 
         # Create the file per each command
-        with open(md_file_path, 'w') as md_file:
+        with open(md_file_path, 'w', encoding='utf-8') as md_file:
             md_file.write(md_template)
+
 
 @click.group()
 def cli():
     pass
+
 
 @cli.command('dumps')
 @click.option('--baseModule', help='The base command module path to import', required=True)
@@ -98,7 +149,9 @@ def cli():
 def dumps(**kwargs):
     """
     # Click-md
-    Create md files per each command, in format of `parent-command`, under the `--docsPath` directory.
+    Create md files per each command,
+    in format of `command.md`,
+    under the `--docsPath` directory.
     """
     base_module = kwargs.get('basemodule')
     base_command = kwargs.get('basecommand')
@@ -117,7 +170,7 @@ def dumps(**kwargs):
     try:
         # Import the base command (group of command) function inside the module
         command_ = getattr(module_, base_command)
-    except:
+    except AttributeError:
         click.echo(f'Could not find command {base_command} on module {base_module}')
         return
 
@@ -132,3 +185,4 @@ def dumps(**kwargs):
 
 
 cli.add_command(cli)
+cli()
